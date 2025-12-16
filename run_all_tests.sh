b9f75c76196519a904d66456a77ec778940d02d1
dipan0saha <dipan.saha@gmail.com>
2025-12-15 23:55:32 -0500

    .

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,5 +2,5 @@
 echo "Running unit/widget tests..."
 flutter test
 echo "Running integration tests..."
-flutter test integration_test
+flutter test integration_test --dart-define=SUPABASE_URL=$SUPABASE_URL --dart-define=SUPABASE_ANON_KEY=$SUPABASE_ANON_KEY
 echo "All tests completed."
